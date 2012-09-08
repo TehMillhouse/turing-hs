2cd17f3c59fa848a5d7fa0757efc38c1a575df7e
Max Wagner <max.wagner01@gmail.com>
2012-09-08 20:17:21 +0200

    Implement show for Automaton, add empty machine constructor

--- turing.hs
+++ turing.hs
@@ -17,3 +17,24 @@ data Automaton = Automaton {
 	q0 :: State,
 	acceptStates :: [State]
 }
+
+
+-- We need to do this to satisfy the Show typeclass
+instance Show Automaton where
+	show (Automaton q tAlpha blank iAlpha _ q0 acceptStates) = 
+		"(" 
+		++ show q 
+		++ ", " 
+		++ show tAlpha 
+		++ ", " 
+		++ show blank 
+		++ ", " 
+		++ show iAlpha 
+		++ ", ð, " 
+		++ show acceptStates 
+		++ ")"
+
+
+-- This is for convenience
+emptyMachine :: Automaton
+emptyMachine = Automaton ["A"] ['a'] ' ' ['a'] (\x y -> ("A", N)) "A" ["A"] 
